5c14fd91af0e1c84b37de7711b5e5e09a007a771
KatrinAnoshka <katrinanoshka@gmail.com>
2019-02-04 13:57:47 +0300

    second commits

--- script.js
+++ script.js
@@ -92,15 +92,16 @@
             paintedhands = select('#paintedHands'),
             paintedhands1 = select('#paintedHands1'),
             realhands = select('#realHands'),
+            hands = selectAll('.hands'),
             cta = select('#cta'),
-            // text1 = select('#text1'),
+            span = selectAll('.text > span')
             text1 = selectAll('#text1 > span'),
             text2 = selectAll('#text2 > span'),
             text3 = selectAll('#text3 > span');            
 
         animate();
         
-        TweenMax.from(canvas, 10, {y:"-=200", ease:Back.easeOut});
+        TweenMax.from(canvas, 5, {y:"-=200", ease:Back.easeOut});
         
         TweenMax.fromTo(canvas, 2, {rotation:2, transformOrigin:"50% -100px"}, {
             rotation:-2, ease:Power2.easeInOut, yoyo:true, repeat:4, onComplete:function(){
@@ -110,45 +111,47 @@
         function animate() {
           
             tl
-           
+            
+            .set([spot, span, hands], {alpha:0})
+            
             .to(wrect, 0.7, {alpha:0, ease:Linear.easeNone})
 
     // Frame-1
             .addLabel("fr", "+=1")
 
-            .to(canvas, 2.6, {opacity:1, ease:Power2.easeOut}, "fr")
+            .to(canvas, 0.2, {opacity:1, ease:Power2.easeOut}, "fr")
             .to(spot, 2, {opacity:1, ease:Power2.easeOut}, "fr+=0.5")
             .staggerTo(text1, 1, {opacity:1, ease:Linear.easeNone}, 0.5, "fr+=3")
-            .from(cta, 1.2, {opacity:1, y:"+=80", ease:Power2.easeOut}, "fr+=4.6")
-            .to(cta, 1.1, {opacity:1}, "fr+=4.8")
+            .from(cta, 0.7, {opacity:1, y:"+=80", ease:Power2.easeOut}, "fr+=4.6")
+            .to(cta, 0.7, {opacity:1}, "fr+=4.8")
 
     // Frame-2 
     
             .addLabel("fr2", "+=1")  
 
             .to(canvas, 1.5, {y:"-=200", ease:Back.easeIn}, "fr2")  
-            .from(paintedhands, 10, {opacity:1, y:"+=180", ease:Power2.easeOut}, "fr2+=1.2")                   
-            .from(paintedhands1, 19, {opacity:0, y:"+=180", ease:Power2.easeOut}, "fr2+=0.8")
-            .to(paintedhands1, 5, {opacity:1, ease:Power2.easeIn}, "fr2+=3.4")
-            .to(paintedhands, 4, {opacity:0}, "fr2+=3.3")
-            .to(text1, 0.8, {opacity:0}, "fr2+=10")
-            .staggerTo(text2, 0.8, {opacity:1, ease:Linear.easeNone}, 0.8, "fr2+=11.2")         
+            .from(paintedhands, 4, {opacity:1, y:"+=180", ease:Power2.easeOut}, "fr2+=0.7")                   
+            .from(paintedhands1, 4, {opacity:0, y:"+=180", ease:Power2.easeOut}, "fr2+=0.1")
+            .to(paintedhands1, 2, {opacity:1, ease:Power2.easeIn}, "fr2+=2.4")
+            .to(paintedhands, 2, {opacity:0}, "fr2+=3.3")
+            .to(text1, 0.8, {opacity:0}, "fr2+=4.2")
+            .staggerTo(text2, 0.8, {opacity:1, ease:Linear.easeNone}, 0.8, "fr2+=5.2")         
             
     // Frame-3 
     
-            .addLabel("fr3", "-=6.5")   
+            .addLabel("fr3", "+=0.5")   
             
             .to(paintedhands1, 3, {opacity:0}, "fr3")
             .to(realhands, 6, {opacity:1, ease:Power2.easeIn}, "fr3-=1")
             .to(bg, 2, {backgroundColor:"#00aeef", delay:0.5}, "fr3+=2.1")
             .to(spotColor, 2, {fill:"rgb(61, 190, 239)", delay:0.5}, "fr3+=2.1")
-            .to(cta, 0.3, {background:"#fff", color:"rgb(0, 174, 239)", delay:0.5}, "fr3+=2")  
-            .to(text2, 2, {opacity:0}, "fr3+=1.5")
+            .to(cta, 0.5, {background:"#fff", color:"rgb(0, 174, 239)", delay:0.5}, "fr3+=2")  
+            .to(text2, 0.5, {opacity:0}, "fr3+=1")
             .to(logoColor, 2, {fill:"#ffffff", ease:Power2.easeIn}, "fr3+=1.6")
             .to(logoText, 2, {color:"#ffffff", ease:Power2.easeIn}, "fr3+=1.6")                      
             .staggerTo(text3, 0.8, {opacity:1, ease:Linear.easeNone}, 0.5, "fr3+=4.8") 
             .to(spotColor, 2, {opacity:0}, "fr3+=4.8")
-            .to(hashtag, 1.8, {text:"#MAKEAPROMISE", ease:Linear.easeNone}), "fr3+=4.9";
+            .to(hashtag, 0.6, {text:"#MAKEAPROMISE", ease:Linear.easeNone}), "fr3+=4.8";
 
         }
 };
